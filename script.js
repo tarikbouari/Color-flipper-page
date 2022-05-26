d686100a8db17ac27661e542f7f85fdbba4b16a8
Rikstone <tarikbouari@gmail.com>
2022-05-26 13:03:33 +0000

    added a javascript file with variables

---
+++ script.js
@@ -0,0 +1,8 @@
+// let declare the hexdecimal array
+let hex =[ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,"A","B","C","D","E","F"];
+
+// declation of variables*
+
+const btn = document.querySelector('.click-button');
+const color = document.querySelector('.colorHex');
+const body = document.querySelector('body');
