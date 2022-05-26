acd72200822a27d3888db62d014aa033937f4982
Rikstone <tarikbouari@gmail.com>
2022-05-26 13:10:35 +0000

    update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // let declare the hexdecimal array
-let hex =[ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,"A","B","C","D","E","F"];
+let hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 // declation of variables*
 
@@ -8,22 +8,21 @@ const color = document.querySelector('.colorHex');
 const body = document.querySelector('body');
 
 
-btn.addEventListener("click", () =>{
+btn.addEventListener("click", () => {
 
     let colorNumbers = '#';
 
-    for(let i=0; i<6 ;i++){
+    for (let i = 0; i < 6; i++) {
 
         colorNumbers += hex[getRandomElement()];
     }
 
-   body.style.backgroundColor = colorNumbers;
-   color.textContent = colorNumbers;
+    body.style.backgroundColor = colorNumbers;
+    color.textContent = colorNumbers;
 
 });
 
-function getRandomElement(){
+function getRandomElement() {
 
-    return Math.floor( Math.random() * hex.length);
+    return Math.floor(Math.random() * hex.length);
 }
-
