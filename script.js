f9668fea9598c35f752e94c1ec826c8f6a32e97e
Rikstone <tarikbouari@gmail.com>
2022-05-26 13:05:07 +0000

    create the addeventlistiner function

--- script.js
+++ script.js
@@ -6,3 +6,24 @@ let hex =[ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,"A","B","C","D","E","F"];
 const btn = document.querySelector('.click-button');
 const color = document.querySelector('.colorHex');
 const body = document.querySelector('body');
+
+
+btn.addEventListener("click", () =>{
+
+    let colorNumbers = '#';
+
+    for(let i=0; i<6 ;i++){
+
+        colorNumbers += hex[getRandomElement()];
+    }
+
+   body.style.backgroundColor = colorNumbers;
+   color.textContent = colorNumbers;
+
+});
+
+function getRandomElement(){
+
+    return Math.floor( Math.random() * hex.length);
+}
+
